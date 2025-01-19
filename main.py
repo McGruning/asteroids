55cd1d2745ed6077952d897feab2b8a5144d1936
McGruning <markus.gruning@outlook.com>
2025-01-19 14:16:32 +1100

    G: forward, backward, copied source

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
 import pygame
 from constants import *
 from player import Player
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    # Create a clock obect to control the game's frame rate
     clock = pygame.time.Clock()
-    dt = 0
-    
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
-    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        # Fill the screen with black colour
-        screen.fill((0, 0, 0))
 
-        # Draw the player
-        player.draw(screen)
+        for obj in updatable:
+            obj.update(dt)
 
-       # Update the player
-        player.update(dt)
-        
-        # Update the display
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
-        # Cap the frame rate to 60 FPS
-        dt = clock.tick(60) / 1000 # Tick method returns milliseconds, so we divide by 1000 to get seconds
-    
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
-   
